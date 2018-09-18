091f69c86fd7eeda0c65da9e64b70c02da33efd7
François Wouts <francois.wouts@airtasker.com>
2018-09-18 14:36:54 +1000

    Split up reducers

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,13 @@
 import produce from "immer";
 import { Action } from "./actions";
-import { EMPTY_STATE, State } from "./state";
+import {
+  CurrentRepoState,
+  EMPTY_STATE,
+  Loadable,
+  RefsState,
+  ReposState,
+  State
+} from "./state";
 
 export const rootReducer = (
   state: State = {
@@ -8,47 +15,68 @@ export const rootReducer = (
   },
   action: Action
 ): State => {
+  return produce(state, draft => {
+    draft.repos = reposReducer(draft.repos, action);
+    draft.currentRepo = currentRepoReducer(draft.currentRepo, action);
+  });
+};
+
+const reposReducer = (
+  reposState: Loadable<ReposState>,
+  action: Action
+): Loadable<ReposState> => {
   switch (action.type) {
     case "UPDATE_REPOS":
-      return produce(state, draft => {
-        draft.repos = action.repos;
-      });
+      return action.repos;
+    default:
+      return reposState;
+  }
+};
+
+const currentRepoReducer = (
+  currentRepoState: CurrentRepoState | undefined,
+  action: Action
+): CurrentRepoState | undefined => {
+  switch (action.type) {
     case "SELECT_REPO":
-      return produce(state, draft => {
-        draft.currentRepo = {
-          owner: action.owner,
-          repo: action.repo,
-          refs: EMPTY_STATE
-        };
-      });
+      return {
+        owner: action.owner,
+        repo: action.repo,
+        refs: EMPTY_STATE
+      };
+    default:
+      if (currentRepoState) {
+        return produce(currentRepoState, draft => {
+          draft.refs = refsReducer(currentRepoState.refs, action);
+        });
+      }
+      return currentRepoState;
+  }
+};
+
+const refsReducer = (
+  refsState: Loadable<RefsState>,
+  action: Action
+): Loadable<RefsState> => {
+  switch (action.type) {
     case "UPDATE_REFS":
-      return produce(state, draft => {
-        if (draft.currentRepo) {
-          draft.currentRepo.refs = action.refs;
-        }
-      });
+      return action.refs;
     case "SELECT_REF":
-      return produce(state, draft => {
-        if (
-          draft.currentRepo &&
-          draft.currentRepo.refs &&
-          draft.currentRepo.refs.status === "loaded"
-        ) {
-          draft.currentRepo.refs.selectedRefName = action.refName;
+      return produce(refsState, draft => {
+        if (draft.status === "loaded") {
+          draft.selectedRefName = action.refName;
         }
       });
     case "UPDATE_COMPARISON":
-      return produce(state, draft => {
+      return produce(refsState, draft => {
         if (
-          draft.currentRepo &&
-          draft.currentRepo.refs &&
-          draft.currentRepo.refs.status === "loaded" &&
-          draft.currentRepo.refs.selectedRefName === action.refName
+          draft.status === "loaded" &&
+          draft.selectedRefName === action.refName
         ) {
-          draft.currentRepo.refs.comparison = action.comparison;
+          draft.comparison = action.comparison;
         }
       });
     default:
-      return state;
+      return refsState;
   }
 };
